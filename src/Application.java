16f61acf54a313a50c443e3faa8db1070686fefc
Peter Fitzgerald <peterfitzgerald81@gmail.com>
2024-02-18 19:28:04 +0000

    Initial commit

---
+++ src/Application.java
@@ -0,0 +1,38 @@
+import java.awt.*;
+import java.awt.event.MouseListener;
+import javax.swing.*;
+
+public class Application extends JFrame implements Runnable, MouseListener {
+
+    public Application(){
+        addMouseListener(this);
+    }
+
+    // mouse events which must be implemented for MouseListener
+    public void mousePressed(MouseEvent e) {
+
+    }
+    public void mouseReleased(MouseEvent e) {
+
+    }
+    public void mouseEntered(MouseEvent e) {
+
+    }
+    public void mouseExited(MouseEvent e) {
+
+    }
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+
+
+
+    public static void amin(String[] args){
+
+    }
+
+
+
+
+}
